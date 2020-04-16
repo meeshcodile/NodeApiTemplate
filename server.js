711ee2ef8317a4ce7bd63fd7d8f612a3941a04c2
“mishealcodile” <ezekielmisheal4@gmail.com>
2020-04-16 01:31:24 +0100

    profile routes api(delete, get and post) request

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const app = express()
-const port =process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
-const mongodb = require('./config/db').MONGOURL
-const bodyparser = require('body-parser')
-const passport = require('passport')
-
+const mongodb = require("./config/db").MONGOURL;
+const bodyparser = require("body-parser");
+const passport = require("passport");
 
 // database connection
 mongoose
-  .connect(mongodb, { useUnifiedTopology: true, useNewUrlParser:true })
+  .connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log("database connected successfully");
   })
@@ -18,26 +17,24 @@ mongoose
   });
 
 // bodyparser middleware
-app.use(bodyparser.json()); 
-app.use(bodyparser.urlencoded({extended:false})) 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 
 // passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // passport congfiguration
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
-const userRoute = require('./routes/api/users')
+const userRoute = require("./routes/api/users");
 const profileRoute = require("./routes/api/profile");
 const postRoute = require("./routes/api/post");
 
-
 // route handling
-app.use('/api/users', userRoute)
+app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postRoute);
 
-
-app.listen(port,(req, res)=>{
-    console.log(`express server started at port ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`express server started at port ${port}`);
+});
